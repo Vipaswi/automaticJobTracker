d5a62b327d944f40362b144ac5eeed556ede2bd2
Vipaswi Thapa <vipa.thapa@gmail.com>
2025-07-09 11:41:34 -0400

    ADded a watch function in server.js and changed the port to 8080

--- api/server.js
+++ api/server.js
@@ -1,14 +1,60 @@
 const express = require("express");
+const { google } = require('googleapis');
 
 const server = express();
+server.use(express.json())
 
-const PORT = 8000; //Dev purposes
+const PORT = 8080; //Dev purposes
+
+/**
+ * Used for event driven api-calls. 
+ * 
+ * @param {*} userAccessToken : The access token for the user
+ * @returns the data historyId
+ */
+async function watchGmail(userAccessToken) {
+  const auth = new google.auth.OAuth2();
+  auth.setCredentials({ access_token: userAccessToken });
+
+  const gmail = google.gmail({ version: 'v1', auth });
+
+  const res = await gmail.users.watch({
+    userId: 'me',
+    requestBody: {
+      topicName: 'projects/automatic-job-tracker/topics/emails',
+      labelIds: ['INBOX'],
+      labelFilterBehavior: include
+    }
+  });
+
+  console.log('Watch response:', res.data);
+  return res.data.historyId;
+}
+
+const fetchMessageList = async () => {
+  try {
+    const res = await fetch("https://gmail.googleapis.com/gmail/v1/users/{userId}/messages");
+  } catch (error) {
+    
+  }
+
+}
 
 // Define Methods:
 
-server.get("token", (req,res) => {
-  
+// The first method is responsible for pinging the server every X minutes, where X changes based on if the user is active or not.
+// It determines if new emails have arrived from the gmail messages.list api, and comparing it to the most recent id received/processed.
+// If there were new emails, it pulls in all the emails and runs them through the parseEmail function.
+server.get("/token", (req,res) => {
+  res.status(200).json({
+    name: "joe"
+  });
 });
+
+server.post("/email", (req,res) => {
+  const {email} = req.body;
+  
+})
 
 
 // Start listening for calls to the server
